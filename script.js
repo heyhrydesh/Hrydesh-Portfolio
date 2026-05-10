5373c243e1de3988ee365bdfb163b27ef03f0709
heyhrydesh <hrydeshdabhi7276@gmail.com>
2026-05-10 16:32:30 +0530

    Fixed responsive layout

--- script.js
+++ script.js
@@ -486,9 +486,7 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", () => {
-
     navLinks.classList.toggle("active");
-
 });
 
 /* CLOSE OUTSIDE */
